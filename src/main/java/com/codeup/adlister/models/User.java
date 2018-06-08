9a1dc472c8caec6338cce9de3c181ee74dbbb17f
danielconner <metalman1342@gmail.com>
2018-06-08 10:48:59 -0500

    i think thats everything

--- src/main/java/com/codeup/adlister/models/User.java
+++ src/main/java/com/codeup/adlister/models/User.java
@@ -1,4 +1,5 @@
 package com.codeup.adlister.models;
+import com.codeup.adlister.util.Password;
 
 public class User {
     private long id;
@@ -8,11 +9,17 @@ public class User {
 
     public User() {}
 
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
     public User(long id, String username, String email, String password) {
         this.id = id;
         this.username = username;
         this.email = email;
-        this.password = password;
+        setPassword(password);
     }
 
     public long getId() {
@@ -44,6 +51,6 @@ public class User {
     }
 
     public void setPassword(String password) {
-        this.password = password;
+        this.password = Password.hash(password);
     }
 }
